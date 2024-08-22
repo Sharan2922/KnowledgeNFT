ab531fc4b8f42e3a63e330e185b88bdf556202f1
Krupa <sharathkariyappa98@gmail.com>
2024-08-22 22:58:50 +0530

    added category component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Style from '../styles/index.module.css'
-import { HeroSection, Service, BigNFTSlider,Subscribe } from '@/components/componentindex'
+import { HeroSection, Service, BigNFTSlider,Subscribe, Title } from '@/components/componentindex'
 
 const Home = () => {
   return (
@@ -8,7 +8,11 @@ const Home = () => {
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title
+      heading="Browse by categoty"
+      paragraph="Explore the NFTs in the most featured categories" />
       <Subscribe />
+     
     </div>
   )
 }
